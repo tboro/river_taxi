247c42df176c617b4b351b651a9fc6513888c712
tboro <tomasz.borodziuk@gmail.com>
2014-09-09 21:12:23 +0200

    River Taxi

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -1,1 +1,1 @@
- : /var/www/html/RT/rivertaxi/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
+ : /var/www/html/RT/river_taxi/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
